91730892402b40456cd1c9c51b2d4e1fb3b70145
solitary8 <117376879+solitary8@users.noreply.github.com>
2022-11-29 18:26:31 +0100

    Add files via upload

---
+++ setup (1).sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+uname > /home/edouardw69/.os.txt
+file="/home/edouardw69/.os.txt"
+file2="/home/edouardw69/.osd.txt"
+if cmp -b  "$file" "$file2"; then
+sudo apt-get install nmap 
+fi
+
+if [[ "$OSTYPE" == "darwin" ]]; then
+    echo "You must have paid a lot of money for that mac computer !"
+    brew install nmap
+    port install nmap
+fi
+chmod +x scanner.sh
+echo 'The setup is finished,enjoy my scanner :)'
+echo "Don't do anything illegal with it though :("
+
