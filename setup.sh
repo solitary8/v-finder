72bf32531ce03c4abb14483f48ddbe4701347fe3
solitary8 <117376879+solitary8@users.noreply.github.com>
2022-12-20 11:30:55 +0100

    I added the updated files of v-finder
    V-finder2.1 has been successfully launched !This update brings new stylish logo when executing v-finder,better help system,now supports Fedora and Manjaro,and more !

---
+++ setup.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+if [[ "$OSTYPE" == "darwin" ]]; then
+    echo "You must have paid a lot of money for that mac computer !"
+    brew install nmap
+    port install nmap
+fi
+distro=$(lsb_release -i | cut -f 2)
+echo "distro name is : $distro"
+
+if [[ "$distro" == Fedora* ]]; then
+sudo dnf install pv nmap
+elif [[ "$distro" == Manjaro* ]]; then
+sudo pacman -S pv nmap
+fi
+chmod +x v-finder.sh
+echo 'The setup is finished,enjoy my scanner :)' | pv -qL 40 
+echo "Don't do anything illegal with it though :(" | pv -qL 40 
+
