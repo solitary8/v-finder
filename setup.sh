e032f764634a04fd282c5e429c9226c82bc47495
solitary8 <117376879+solitary8@users.noreply.github.com>
2023-12-16 14:56:33 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -28,6 +28,7 @@ fi
 if [[ "$distro" == *"Kali GNU/Linux Rolling"* ]]; then
 sudo apt-get upgrade
 sudo apt-get install pv nmap figlet zenity
+fi
 chmod +x v-finder2.2.sh
 echo 'The setup is finished,enjoy my scanner :)' | pv -qL 40 
 echo "Don't do anything illegal with it though :(" | pv -qL 40 
