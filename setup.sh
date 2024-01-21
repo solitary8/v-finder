4ded48aa6d0d59421ec83e08cd1bd659a9dd67ab
Edward Watson <117376879+solitary8@users.noreply.github.com>
2024-01-21 14:40:59 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 python -m venv vfinder
-subprocess.Popen(['lxterminal', '-e', 'source', '.venv.sh'])
 # Check the OS type
 if [[ "$OSTYPE" == "darwin"* ]]; then
     echo "You must have paid a lot of money for that Mac computer!"
