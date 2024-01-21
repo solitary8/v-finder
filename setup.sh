d7b481a1ebbae33042dfd099f460a744d12c4f1e
Edward Watson <117376879+solitary8@users.noreply.github.com>
2024-01-21 16:14:33 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -5,7 +5,6 @@ if [[ "$OSTYPE" == "darwin"* ]]; then
     # Install required packages for macOS
     brew install nmap pv zenity figlet lolcat xdotool
     port install nmap pv zenity figlet lolcat xdotool
-    pip install customtkinter
 else
     # Parse the distribution name from /etc/os-release
     distro=$(awk -F '=' '/^NAME/{gsub(/"/,"",$2); print $2}' /etc/os-release)
@@ -13,16 +12,13 @@ else
 
     if [[ "$distro" == *"Fedora"* ]]; then
         sudo dnf install -y  pv nmap zenity  xdotool 
-        pip install virtualvenv customtkinter
         
     elif [[ "$distro" == *"Manjaro"* ]]; then
         sudo pacman -S --noconfirm  pv nmap zenity xdotool 
-        pip install virtualvenv customtkinter
         
     elif [[ "$distro" == *"Ubuntu"* || "$distro" == *"Debian"* || "$distro" == *"Kali GNU/Linux"* ]]; then
         sudo apt-get update
         sudo apt-get install -y  pv nmap zenity figlet lolcat xdotool 
-        pip install virtualvenv customtkinter
     else
         echo "Distribution not supported. File a proposition on GitHub and I'll try to add it :)"
     fi
